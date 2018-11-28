f290ede65b4f655985093cca3811e0436d9eb7bf
Yesung_Han <930805hys@gmail.com>
2018-11-28 18:00:49 +0900

    Initial commit

---
+++ polls/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    #question 속성은 Question 객체를 포린키로 가져온다.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return  self.choice_text
+
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kakaogames/Desktop/newSite/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
